bdc3148acce3288c0d08a732b13d4c0c71dc9d06
Silvano Cerza <silvanocerza@gmail.com>
2015-08-13 11:55:47 +0200

    Token is now sent to backend when necessary

--- app/src/main/java/it/silvanocerza/gcmproofofconcept/RegistrationIntentService.java
+++ app/src/main/java/it/silvanocerza/gcmproofofconcept/RegistrationIntentService.java
@@ -3,13 +3,17 @@ package it.silvanocerza.gcmproofofconcept;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.provider.Settings;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
+import com.google.gson.JsonObject;
 
 import java.io.IOException;
+
+import retrofit.client.Response;
 
 public class RegistrationIntentService extends IntentService {
     public static final String TAG = "RegIntentService";
@@ -27,13 +31,27 @@ public class RegistrationIntentService extends IntentService {
             String token = instanceID.getToken(getString(R.string.project_number),
                     GoogleCloudMessaging.INSTANCE_ID_SCOPE);
 
+            String savedToken = preferences.getString(Constants.TOKEN, "");
+
             Log.i(TAG, "GCM Registration token: " + token);
 
-            preferences.edit().putString(Constants.TOKEN, token).apply();
+            if (savedToken.equals("")) {
 
-            sendRegistrationToServer(token);
+                preferences.edit().putString(Constants.TOKEN, token).apply();
 
-            preferences.edit().putBoolean(Constants.TOKEN_IS_SENT, true).apply();
+                sendRegistrationToServer(token);
+
+                preferences.edit().putBoolean(Constants.TOKEN_IS_SENT, true).apply();
+
+            } else if (!token.equals(savedToken)) {
+
+                preferences.edit().putString(Constants.TOKEN, token).apply();
+
+                updateRegistrationToServer(token);
+
+                preferences.edit().putBoolean(Constants.TOKEN_IS_SENT, true).apply();
+
+            }
 
             Intent registrationComplete = new Intent(Constants.REGISTRATION_COMPLETE);
             LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
@@ -45,8 +63,35 @@ public class RegistrationIntentService extends IntentService {
         }
     }
 
-
     private void sendRegistrationToServer(String token) {
-        // noop
+        JsonObject json = new JsonObject();
+
+        String deviceId = Settings.Secure.getString(getBaseContext().getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+
+        json.addProperty("device_id", deviceId);
+        json.addProperty("registration_id", token);
+
+        Response response = new RestClient().getApi().postRegistrationId(json);
+
+        if (response.getStatus() != 201) {
+            sendRegistrationToServer(token);
+        }
+    }
+
+    private void updateRegistrationToServer(String token) {
+        JsonObject json = new JsonObject();
+
+        String deviceId = Settings.Secure.getString(getBaseContext().getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+        json.addProperty("registration_id", token);
+
+        Response response = new RestClient().getApi().updateRegistrationId(deviceId, json);
+
+        if (response.getStatus() != 200) {
+            sendRegistrationToServer(token);
+        }
     }
 }
